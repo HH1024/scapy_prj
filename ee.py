9705f989c8f41fe90a5f43d371c77a43368571a1
horbie <horbie_huo@163.com>
2017-04-28 10:31:07 +0800

    scrapy baike

--- ee.py
+++ ee.py
@@ -3,3 +3,8 @@ from pymongo import MongoClient
 connection=MongoClient('localhost',27017) 
 db=connection.scapy_baike
 print(db)
+urls_collection=db.urls
+data = urls_collection.find_one({'used': False})
+print(data)
+data = urls_collection.find_one({'url': "asasas"})
+print(data)
