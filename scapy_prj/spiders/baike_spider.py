2c7bff67219c9a06fee191c840cecc5bcd28e02b
horbie <horbie_huo@163.com>
2017-04-28 10:31:07 +0800

    scrapy baike

--- scapy_prj/spiders/baike_spider.py
+++ scapy_prj/spiders/baike_spider.py
@@ -3,6 +3,9 @@
 import scrapy
 import re, time
 import random
+import zlib
+import bson.binary
+from datetime import datetime
 
 from pymongo import MongoClient
 connection=MongoClient('localhost',27017) 
@@ -16,6 +19,8 @@ content_page_collection=db.content_page
 
 __READY_FOR_SPIDER__ = []
 __URL_PREFIX__ = 'https://baike.baidu.com'
+__URL_PREFIX_LEN__ = len(__URL_PREFIX__) + 3
+__BAIKE_DOMAIN__ = 'baike.baidu.com'
 file_suffix = [
     '.css',
     '.js',
@@ -32,29 +37,61 @@ class BaikeSpider(scrapy.Spider):
             # 'https://baike.baidu.com/',
             r'https://baike.baidu.com/tashuo/browse/content?id=f78312c88b49b6e8f9fbf95f'
         ]
-        yield scrapy.Request(url='https://baike.baidu.com/' , callback=self.parse)
+        data = urls_collection.find_one({'used': False})
+        if data:
+            self.log("first url %s" % data['url'])
+            urls_collection.update(
+                data,
+                {'used': True, 'updated_at': datetime.now()},
+                upsert = False
+            )
+            yield scrapy.Request(url=data['url'] , callback=self.parse)
+        else:
+            self.log("first url %s" % ('https://baike.baidu.com/'))
+            urls_collection.insert({
+                'url': 'https://baike.baidu.com/',
+                'used': True,
+                'created_at': datetime.now()
+            })
+            yield scrapy.Request(url='https://baike.baidu.com/' , callback=self.parse)
         while 1:
-            time.sleep(2 + random.random() * 10)
+            sleep_time = 8 + random.random() * 10
             data = urls_collection.find_one({'used': False})
+            self.log("get next: %s" % data)
             if data:
+                self.log("after %s next url %s" % (sleep_time, data['url']))
+                time.sleep(sleep_time)
+                urls_collection.update(
+                    data,
+                    {'used': True, 'updated_at': datetime.now()},
+                    upsert = False
+                )
                 yield scrapy.Request(url=data['url'] , callback=self.parse)
             else:
+                self.log("after %s next url %s" % (sleep_time, data))
                 break
+                # time.sleep(sleep_time)
         # for url in urls:
         #     yield scrapy.Request(url=url , callback=self.parse)
 
     def parse(self, response):
-        urls_collection.update(
-            {'url': response.url},
-            {'url': response.url, 'used': True},
-            upsert=True
-        )
-        self.log('---'*20)
-        self.log(response.url)
-        self.log(len(response.body))
-        # urls=re.findall(r'<a.*?href=.*?<\/a>', response.body, re.I|re.S|re.M)
-        # res_url = r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')"
-        link_list = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", response.body)
+        self.log("body size %s, url:%s" % (len(response.body), response.url))
+        data = content_page_collection.find({'url': response.url})
+        if not data or data.count() == 0:
+            z_body = zlib.compress(response.body)
+            self.log("compress %s" % len(z_body))
+            content_page_collection.insert({
+                'url': response.url,
+                'page_content': bson.binary.Binary(z_body),
+                'created_at': datetime.now()
+            })
+        else:
+            self.log("repeat scrapy %s" % response.url)
+        urls=re.findall(r'<a.*?href=.*?<\/a>', response.body, re.I|re.S|re.M)
+        res_url = r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')"
+        link_list = []
+        for a_url in urls:
+            link_list.extend(re.findall(res_url, a_url))
         # self.log(link_list)
         for url in link_list:
             if '#' in url:
@@ -67,20 +104,28 @@ class BaikeSpider(scrapy.Spider):
             if is_continue:
                 continue
             if url[0] == '/':
+                # self.log("INNNN %s" % url)
                 self.__addOneChildUrl__(__URL_PREFIX__+url)
                 continue
-            if __URL_PREFIX__ in url:
+            if __BAIKE_DOMAIN__ in url[:__URL_PREFIX_LEN__]:
+                # self.log("INNNN %s" % url)
                 self.__addOneChildUrl__(url)
+            # else:
+            #     self.log("%s not in %s" % (__BAIKE_DOMAIN__, url))
+            # self.log("cannot --- %s" % url)
+            # self.log(type(url))
         # self.log('Saved file %s' % filename)
 
     def __addOneChildUrl__(self, url):
         data = urls_collection.find({'url': url})
-        if not data:
-            self.log("add url:%s" % url)
+        if not data or data.count() == 0:
+            # self.log("add url:%s" % url)
             urls_collection.insert({
                 'url': url,
-                'used': False
+                'used': False,
+                'created_at': datetime.now()
             })
+
         # global __READY_FOR_SPIDER__
         # __READY_FOR_SPIDER__.append(url)
         # if len(__READY_FOR_SPIDER__) >= 10:
